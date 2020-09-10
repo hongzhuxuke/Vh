7c6a58ed3696fdafb6808c0a0e2373d4b95a597b
ke.xu <ke.xu@vhall.com>
2020-09-10 16:15:12 +0800

    del

--- common/UACUtility.h
+++
@@ -1,5 +0,0 @@
-
-
-bool ElevatePrivileges(const wchar_t* exeFilePath, const wchar_t* lpParameters);
-BOOL IsRunAsAdmin();
-bool RunProcess(const wchar_t* exeFilePath, const wchar_t* lpParameters);
--- common/pub.Struct.cpp
+++
@@ -1,5 +0,0 @@
-#include "pub.Struct.h"
-
-STRU_MAINUI_LOG::STRU_MAINUI_LOG(){
-	memset(this, 0, sizeof(STRU_MAINUI_LOG));
-}
